ca64e34107c2a5761571a4b255c3f73ba6dfedc7
Nicholas Rees <70386214+nickreesdev@users.noreply.github.com>
2021-06-02 17:35:51 -0500

    Silly me, forgot to enable kits for Crystal Wars.

--- Game/CrystalWars/src/main/java/org/madblock/crystalwars/CrystalWarsPlugin.java
+++ Game/CrystalWars/src/main/java/org/madblock/crystalwars/CrystalWarsPlugin.java
@@ -4,10 +4,13 @@ import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
 import lombok.Getter;
 import org.madblock.crystalwars.game.CrystalWarsGame;
+import org.madblock.crystalwars.game.kit.ArcherKit;
+import org.madblock.crystalwars.game.kit.WarriorKit;
 import org.madblock.newgamesapi.NewGamesAPI1;
 import org.madblock.newgamesapi.game.GameHandler;
 import org.madblock.newgamesapi.game.GameID;
 import org.madblock.newgamesapi.game.GameProperties;
+import org.madblock.newgamesapi.kits.KitGroup;
 
 /**
  * @author Nicholas
@@ -19,7 +22,7 @@ public class CrystalWarsPlugin extends PluginBase {
     public void onEnable() {
         instance = this;
 
-//        KitGroup kitGroup = new KitGroup("crystalwars", "Crystal Wars", true)
+        KitGroup kitGroup = new KitGroup("crystalwars", "Crystal Wars", true, new WarriorKit(), new ArcherKit());
 
         GameProperties regularCrystalWarsProperties = new GameProperties(GameHandler.AutomaticWinPolicy.MANUAL_CALLS_ONLY)
                 .setCanWorldBeManipulated(true)
@@ -35,5 +38,7 @@ public class CrystalWarsPlugin extends PluginBase {
         NewGamesAPI1.getGameRegistry().registerGame(new GameID(CrystalWarsConstants.BASE_GAME_ID, CrystalWarsConstants.BASE_GAME_SERVER_ID,
                 "Crystal Wars Regular", "Destory the other crystals, but make sure to defend your own!",
                 "crystalwars", new String[] { "crystalwars_regular" }, 1, regularCrystalWarsProperties, CrystalWarsGame.class));
+
+        NewGamesAPI1.getKitRegistry().registerKitGroup(kitGroup);
     }
 }
