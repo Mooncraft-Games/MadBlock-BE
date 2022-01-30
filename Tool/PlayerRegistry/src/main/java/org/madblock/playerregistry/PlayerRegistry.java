dada7af81481edcec89818ba5ead9c742849fc2a
Will <16578982+CloudG360@users.noreply.github.com>
2022-01-30 00:07:06 +0000

    Cleanup of PlayerRegistry including:  - Deprecation of IP/Port "Location" + its no longer stored in tables  - Simplified + Reformated the class, taking away unnecessary checks and condensing optionals.  - Added the pending links table + appended the discord id

--- Tool/PlayerRegistry/src/main/java/org/madblock/playerregistry/PlayerRegistry.java
+++ Tool/PlayerRegistry/src/main/java/org/madblock/playerregistry/PlayerRegistry.java
@@ -18,212 +18,157 @@ import java.util.Optional;
 
 public class PlayerRegistry extends PluginBase implements Listener {
 
+    // Note: Discord IDs do not have a fixed length as they're the millis since jan 1st 2015! Currently, they're 17/18 chars, but I'm setting it to 28 for future-proofing.
+    private static final String CREATE_PLAYER_LOOKUP_TABLE = "CREATE TABLE IF NOT EXISTS player_lookup ( xuid VARCHAR(16) NOT NULL PRIMARY KEY, username VARCHAR(12) NOT NULL, discord_id VARCHAR(28));";
+
+    // Used for linking services such as discord and the minecraft server.
+    // service - short id - examples such as "discord", "minecraft", ... - If it's "minecraft" it can link to anything. If it's anything else, it must be linked back to minecraft unless we introduce forums.
+    // identifier - user identifier - the id given to a user for that specific service (minecraft -> [add xuid], discord -> [add discord id])
+    // code - the code required on the other service for a successful link.
+    private static final String CREATE_SERVICE_LINK_TABLE = "CREATE TABLE IF NOT EXISTS pending_service_links ( service VARCHAR(10) NOT NULL, identifier VARCHAR(32) NOT NULL, code VARCHAR(9), PRIMARY KEY (service, identifier) );";
+
+
     private static final String SELECT_PLAYER_QUERY = "SELECT 1 FROM player_lookup WHERE xuid=?";
-    private static final String RETURNING_PLAYER_QUERY = "UPDATE player_lookup SET username=?, server_ip=?, server_port=? WHERE xuid=?";
-    private static final String NEW_PLAYER_JOIN_QUERY = "INSERT INTO player_lookup (xuid, username, server_ip, server_port) VALUES (?, ?, ?, ?)";
-    private static final String PLAYER_LEAVING_QUERY = "UPDATE player_lookup SET server_ip=NULL, server_port=NULL WHERE xuid=?";
-    private static final String CREATE_PLAYER_LOOKUP_TABLE = "CREATE TABLE IF NOT EXISTS player_lookup ( xuid VARCHAR(16) NOT NULL PRIMARY KEY, username VARCHAR(12) NOT NULL, server_ip VARCHAR(15), server_port INT );";
+    private static final String PLAYER_JOIN_QUERY = "INSERT INTO player_lookup (xuid, username) VALUES (?, ?) ON DUPLICATE KEY UPDATE player_lookup SET username=?;";
+
     private static final String GET_XUID_QUERY = "SELECT xuid FROM player_lookup WHERE UPPER(username)=UPPER(?)";
     private static final String GET_USERNAME_QUERY = "SELECT username FROM player_lookup WHERE xuid=?";
-    private static final String GET_LOCATION_BY_NAME_QUERY = "SELECT server_ip, server_port FROM player_lookup WHERE UPPER(username)=UPPER(?)";
-    private static final String GET_LOCATION_BY_XUID_QUERY = "SELECT server_ip, server_port FROM player_lookup WHERE xuid=?";
 
     @Override
     public void onEnable() {
 
         ConnectionWrapper wrapper;
+
         try {
             wrapper = DatabaseAPI.getConnection("MAIN");
         } catch (SQLException connectionException) {
-            getLogger().error("Failed to establish connection to MAIN database. Disabling...");
-            getLogger().error(connectionException.toString());
-            getServer().getPluginManager().disablePlugin(this);
+            this.getLogger().error("Failed to establish connection to MAIN database. Disabling...");
+            this.getLogger().error(connectionException.toString());
+            this.getServer().getPluginManager().disablePlugin(this);
             return;
         }
+
+        PreparedStatement stmtPlayerLookup = null;
+        PreparedStatement stmtTableLink = null;
+
         try {
-            PreparedStatement stmt = wrapper.prepareStatement(new DatabaseStatement(CREATE_PLAYER_LOOKUP_TABLE));
-            stmt.execute();
+            stmtPlayerLookup = wrapper.prepareStatement(new DatabaseStatement(CREATE_PLAYER_LOOKUP_TABLE));
+            stmtPlayerLookup.execute();
+
+            stmtTableLink = wrapper.prepareStatement(new DatabaseStatement(CREATE_SERVICE_LINK_TABLE));
+            stmtTableLink.execute();
+
+
+
         } catch (SQLException initException) {
-            getLogger().error("Failed to create player_lookup table. Disabling...");
-            getLogger().error(initException.toString());
-            getServer().getPluginManager().disablePlugin(this);
+            this.getLogger().error("Failed to create player_lookup table. Disabling...");
+            this.getLogger().error(initException.toString());
+
+            DatabaseUtility.closeQuietly(stmtPlayerLookup);
+            DatabaseUtility.closeQuietly(stmtTableLink);
+            DatabaseUtility.closeQuietly(wrapper);
+
+            this.getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        getServer().getPluginManager().registerEvents(this, this);
+
+        DatabaseUtility.closeQuietly(stmtPlayerLookup);
+        DatabaseUtility.closeQuietly(stmtTableLink);
         DatabaseUtility.closeQuietly(wrapper);
+
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
+
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(final PlayerJoinEvent event) {
         String xuid = event.getPlayer().getLoginChainData().getXUID();
         String username = event.getPlayer().getLoginChainData().getUsername();
-        String ip = getServer().getIp();
-        int port = getServer().getPort();
-        getServer().getScheduler().scheduleTask(this, () -> {
 
+        getServer().getScheduler().scheduleTask(this, () -> {
             ConnectionWrapper wrapper;
+
             try {
                 wrapper = DatabaseAPI.getConnection("MAIN");
             } catch (SQLException connectionException) {
-                getLogger().warning("Failed to establish connection to update player registry data.");
+                this.getLogger().warning("Failed to establish connection to update player registry data.");
                 return;
             }
-
-            PreparedStatement selectPlayerStmt = null;
-            boolean exists;
-            try {
-                selectPlayerStmt = wrapper.prepareStatement(new DatabaseStatement(SELECT_PLAYER_QUERY, new Object[]{ xuid }));
-                exists = selectPlayerStmt.executeQuery().next();
-            } catch (SQLException selectException) {
-                getLogger().error("An error occurred while retrieving player registry data.");
-                getLogger().error(selectException.toString());
-                if (selectPlayerStmt != null) {
-                    DatabaseUtility.closeQuietly(selectPlayerStmt);
-                }
-                DatabaseUtility.closeQuietly(wrapper);
-                return;
-            }
-            DatabaseUtility.closeQuietly(selectPlayerStmt);
 
             PreparedStatement updateStmt = null;
             try {
-                if (exists) {
-                    updateStmt = wrapper.prepareStatement(new DatabaseStatement(RETURNING_PLAYER_QUERY, new Object[]{ username, ip, port, xuid}));
-                } else {
-                    updateStmt = wrapper.prepareStatement(new DatabaseStatement(NEW_PLAYER_JOIN_QUERY, new Object[]{ xuid, username, ip, port }));
-                }
+                updateStmt = wrapper.prepareStatement(new DatabaseStatement(PLAYER_JOIN_QUERY, new Object[]{ username, xuid, username }));
                 updateStmt.executeUpdate();
+
             } catch (SQLException updateException) {
-                getLogger().error("An error occurred while updating player registry data.");
-                getLogger().error(updateException.toString());
+                this.getLogger().error("An error occurred while updating player registry data.");
+                this.getLogger().error(updateException.toString());
             }
-            if (updateStmt != null) {
-                DatabaseUtility.closeQuietly(updateStmt);
-            }
+
+            DatabaseUtility.closeQuietly(updateStmt);
             DatabaseUtility.closeQuietly(wrapper);
+
         }, true);
     }
 
-    @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent event) {
-        String xuid = event.getPlayer().getLoginChainData().getXUID();
-        getServer().getScheduler().scheduleTask(this, () -> {
-            ConnectionWrapper wrapper;
-            try {
-                wrapper = DatabaseAPI.getConnection("MAIN");
-            } catch (SQLException connectionException) {
-                getLogger().warning("Failed to establish connection to update player registry data.");
-                return;
-            }
-
-            PreparedStatement updateStmt;
-            try {
-                updateStmt = wrapper.prepareStatement(new DatabaseStatement(PLAYER_LEAVING_QUERY, new Object[]{ xuid }));
-                updateStmt.execute();
-            } catch (SQLException exception) {
-                getLogger().error("An error occurred while removing player location data");
-                getLogger().error(exception.toString());
-                return;
-            }
-            if (updateStmt != null) {
-                DatabaseUtility.closeQuietly(updateStmt);
-            }
-            DatabaseUtility.closeQuietly(wrapper);
-        }, true);
-    }
 
     public static Optional<String> getPlayerNameByXuid(String xuid) throws SQLException {
         ConnectionWrapper wrapper = DatabaseAPI.getConnection("MAIN");
         PreparedStatement getUsernameStatement = null;
         String username = null;
+
         try {
             getUsernameStatement = wrapper.prepareStatement(new DatabaseStatement(GET_USERNAME_QUERY, new Object[]{ xuid }));
             ResultSet results = getUsernameStatement.executeQuery();
-            if (results.next()) {
+
+            if (results.next())
                 username = results.getString("username");
-            }
+
         } catch (SQLException statementException) {
-            if (getUsernameStatement != null) {
-                DatabaseUtility.closeQuietly(getUsernameStatement);
-            }
+            DatabaseUtility.closeQuietly(getUsernameStatement);
             DatabaseUtility.closeQuietly(wrapper);
             throw statementException;
         }
+
         DatabaseUtility.closeQuietly(getUsernameStatement);
         DatabaseUtility.closeQuietly(wrapper);
-        if (username != null) {
-            return Optional.of(username);
-        }
-        return Optional.empty();
+
+        return Optional.ofNullable(username);
     }
 
     public static Optional<String> getPlayerXuidByName(String username) throws SQLException {
         ConnectionWrapper wrapper = DatabaseAPI.getConnection("MAIN");
         PreparedStatement getXuidStatement = null;
         String xuid = null;
+
         try {
             getXuidStatement = wrapper.prepareStatement(new DatabaseStatement(GET_XUID_QUERY, new Object[]{ username }));
             ResultSet results = getXuidStatement.executeQuery();
-            if (results.next()) {
+
+            if (results.next())
                 xuid = results.getString("xuid");
-            }
+
         } catch (SQLException statementException) {
-            if (getXuidStatement != null) {
-                DatabaseUtility.closeQuietly(getXuidStatement);
-            }
+
+            DatabaseUtility.closeQuietly(getXuidStatement);
             DatabaseUtility.closeQuietly(wrapper);
             throw statementException;
         }
+
         DatabaseUtility.closeQuietly(getXuidStatement);
         DatabaseUtility.closeQuietly(wrapper);
-        if (xuid != null) {
-            return Optional.of(xuid);
-        }
+
+        return Optional.ofNullable(xuid);
+    }
+
+    @Deprecated
+    public static Optional<PlayerServerLocation> getPlayerLocationByXuid(String xuid) throws SQLException {
         return Optional.empty();
     }
 
-    public static Optional<PlayerServerLocation> getPlayerLocationByXuid(String xuid) throws SQLException {
-        ConnectionWrapper wrapper = DatabaseAPI.getConnection("MAIN");
-        PreparedStatement getLocationByXuidStmt = null;
-        PlayerServerLocation location = null;
-        try {
-            getLocationByXuidStmt = wrapper.prepareStatement(new DatabaseStatement(GET_LOCATION_BY_XUID_QUERY, new Object[]{ xuid }));
-            ResultSet results = getLocationByXuidStmt.executeQuery();
-            if (results.next() && results.getString("server_ip") != null && results.getString("server_port") != null) {
-                location = new PlayerServerLocation(results.getString("server_ip"), results.getInt("server_port"));
-            }
-        } catch (SQLException statementException) {
-            if (getLocationByXuidStmt != null) {
-                DatabaseUtility.closeQuietly(getLocationByXuidStmt);
-            }
-            DatabaseUtility.closeQuietly(wrapper);
-            throw statementException;
-        }
-        DatabaseUtility.closeQuietly(getLocationByXuidStmt);
-        DatabaseUtility.closeQuietly(wrapper);
-        return Optional.ofNullable(location);
-    }
-
+    @Deprecated
     public static Optional<PlayerServerLocation> getPlayerLocationByName(String name) throws SQLException {
-        ConnectionWrapper wrapper = DatabaseAPI.getConnection("MAIN");
-        PreparedStatement getLocationByXuidStmt = null;
-        PlayerServerLocation location = null;
-        try {
-            getLocationByXuidStmt = wrapper.prepareStatement(new DatabaseStatement(GET_LOCATION_BY_NAME_QUERY, new Object[]{ name }));
-            ResultSet results = getLocationByXuidStmt.executeQuery();
-            if (results.next() && results.getString("server_ip") != null && results.getString("server_port") != null) {
-                location = new PlayerServerLocation(results.getString("server_ip"), results.getInt("server_port"));
-            }
-        } catch (SQLException statementException) {
-            if (getLocationByXuidStmt != null) {
-                DatabaseUtility.closeQuietly(getLocationByXuidStmt);
-            }
-            DatabaseUtility.closeQuietly(wrapper);
-            throw statementException;
-        }
-        DatabaseUtility.closeQuietly(getLocationByXuidStmt);
-        DatabaseUtility.closeQuietly(wrapper);
-        return Optional.ofNullable(location);
+        return Optional.empty();
     }
 
 }
